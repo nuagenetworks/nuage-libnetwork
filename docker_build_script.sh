99ab3171c40d0dfb02f8dc71d52826bdb8141b4d
Rajesh Babu Mugunthu Maheswaran <rajesh_babu.mugunthu_maheswaran@nokia.com>
2018-02-01 00:44:11 -0800

    build in container

---
+++ docker_build_script.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -x
+
+USER_ID=$(id -u)
+GROUP_ID=$(id -g)
+DOCKERFILE="Dockerfile.build"
+PWD=$(pwd)
+docker run --privileged \
+    -e NUAGE_PROJECT=${NUAGE_PROJECT} \
+    -e NUAGE_BUILD_RELEASE=${NUAGE_BUILD_RELEASE} \
+    -e NUAGE_BUILD_NUMBER=${NUAGE_BUILD_NUMBER} \
+    -e version=${NUAGE_PROJECT}.${NUAGE_BUILD_RELEASE} \
+    -e GOPATH=/BUILD/go \
+    -e USER_ID=${USER_ID} \
+    -e GROUP_ID=${GROUP_ID} \
+    -v /home/rajesh/workspace/nuage-libnetwork:/BUILD/go/src/github.com/nuagenetworks/nuage-libnetwork \
+    -v /usr/global:/usr/global \
+    -v /root:/root \
+    libnet \
+    sh scripts/buildRPM.sh
