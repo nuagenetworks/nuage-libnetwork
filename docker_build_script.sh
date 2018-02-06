8dee460624d5dc77d9da2827b14a21469fa3c60b
Rajesh Babu Mugunthu Maheswaran <rajesh_babu.mugunthu_maheswaran@nokia.com>
2018-02-06 01:04:15 -0800

    mount Docker socket

--- docker_build_script.sh
+++ docker_build_script.sh
@@ -21,5 +21,6 @@ docker run --privileged \
     -v ${PWD}:/BUILD/go/src/github.com/nuagenetworks/nuage-libnetwork \
     -v /usr/global:/usr/global \
     -v /root:/root \
+    -v /var/run/docker.sock:/var/run/docker.sock \
     registry.mv.nuagenetworks.net:5000/build/nuage-libnetwork \
     make ${MAKE_TARGET}
